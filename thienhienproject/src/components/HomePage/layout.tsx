60449e8974ab8986f7addae21920d656adffa4fd
thien <thien@gkim.vn>
2024-12-01 01:10:35 +0700

    update package

--- thienhienproject/src/components/HomePage/layout.tsx
+++ thienhienproject/src/components/HomePage/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Header } from "./header"
 import { Footer } from "./footer"
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 interface LayoutProps {
   children: React.ReactNode
 }
@@ -10,6 +10,7 @@ export function Layout({ children }: LayoutProps) {
   return (
     <div id = "homepage" className="min-h-screen flex flex-col">
       <Header />
+      <SpeedInsights/>
       <main className="flex-1">
         {children}
       </main>
